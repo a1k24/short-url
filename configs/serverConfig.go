d2f61be3752451527c1fa2cf814387ca67876b40
a1k24 <akashmandal001@gmail.com>
2020-09-07 02:08:23 +0530

    Added Dockerfile.

--- configs/serverConfig.go
+++ configs/serverConfig.go
@@ -21,7 +21,7 @@ func Init() {
 	flag.StringVar(&username, "username", "username", "user name for mongodb")
 	flag.StringVar(&password, "password", "password", "password for mongodb")
 	flag.StringVar(&DBName, "dbname", "globaldb", "database name for mongodb")
-	flag.StringVar(&baseUrl, "baseurl", "localhost:10000", "base url for server")
+	flag.StringVar(&baseUrl, "baseurl", "0.0.0.0:10000", "base url for server")
 	flag.StringVar(&dbHost, "dbhost", "test-cluster.f8tgw.mongodb.net", "host for mongo server")
 	flag.Parse()
 	MongoUrl = fmt.Sprintf("mongodb+srv://%s:%s@%s/%s?retryWrites=true&w=majority", username, password, dbHost, DBName)
